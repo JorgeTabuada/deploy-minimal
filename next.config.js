da67db76fff769277ca1106652cad2b73197a922
JorgeTabuada <jorgetabuada@airpark.pt>
2025-06-03 02:10:08 +0100

    ⚙️ Adicionar configurações TypeScript, Next.js e exemplo de environment

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  
+  // Configuração mínima e estável
+  images: {
+    domains: ['ioftqsvjqwjeprsckeym.supabase.co']
+  }
+};
+
+module.exports = nextConfig;
